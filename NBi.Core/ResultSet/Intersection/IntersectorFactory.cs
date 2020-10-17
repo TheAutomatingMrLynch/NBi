f1212dbb4ea9a405fae129aaa65855936f84a90b
Cédric L. Charlier <cedric.charlier@yucom.be>
2020-10-17 19:17:49 +0200

    Engine for intersection detection

---
+++ NBi.Core/ResultSet/Intersection/IntersectorFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.ResultSet.Intersection
+{
+    public class IntersectorFactory
+    {
+        public Intersector Instantiate(ISettingsResultSet settings)
+        {
+            switch(settings)
+            {
+                case SettingsOrdinalResultSet o: return new OrdinalIntersector(o);
+                case SettingsNameResultSet n: return new NameIntersector(n);
+                default: throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
