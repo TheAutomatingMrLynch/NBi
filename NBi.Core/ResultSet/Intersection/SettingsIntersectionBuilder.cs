c403cbf9a9c10dbd6a0dcbf35d0a05176f505609
Cédric L. Charlier <cedric.charlier@yucom.be>
2020-10-17 19:17:49 +0200

    Engine for intersection detection

---
+++ NBi.Core/ResultSet/Intersection/SettingsIntersectionBuilder.cs
@@ -0,0 +1,26 @@
+﻿using NBi.Core.Scalar.Comparer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.ResultSet.Intersection
+{
+    public class SettingsIntersectionBuilder : SettingsResultSetBuilder
+    {
+        protected override void OnBuild()
+        {
+            BuildSettings(ColumnType.Text, ColumnType.Numeric, null);
+        }
+
+        protected override void OnCheck()
+        {
+            PerformInconsistencyChecks();
+            PerformSetsAndColumnsCheck(
+                SettingsOrdinalResultSet.KeysChoice.All
+                , SettingsOrdinalResultSet.ValuesChoice.None);
+            PerformDuplicationChecks();
+        }
+    }
+}
